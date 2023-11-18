2535f15ecd2634b36daa8514b72bd02186860ba7
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-18 17:38:22 +0600

    Updated Find the Highest Altitude

--- FindtheHighestAltitude.cpp
+++ FindtheHighestAltitude.cpp
@@ -6,14 +6,17 @@ class Solution
 public:
     int largestAltitude(vector<int> &gain)
     {
-        vector<int> altitudes = {0};
+        int max = 0;
         int sum = 0;
         for (int i = 0; i < gain.size(); i++)
         {
             sum += gain[i];
-            altitudes.push_back(sum);
+            if (sum > max)
+            {
+                max = sum;
+            }
         }
-        return *max_element(altitudes.begin(), altitudes.end());
+        return max;
     }
 };
 int main()
