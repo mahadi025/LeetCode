0a97afc6f10d48f9bf3803a1768836b37840de49
mahadi025 <mahadikarimmunif@gmail.com>
2023-11-30 21:34:11 +0600

    Solved Number of Recent Calls

---
+++ NumberofRecentCalls.cpp
@@ -0,0 +1,19 @@
+class RecentCounter {
+public:
+	RecentCounter() {
+
+	}
+
+	queue<int> counter;
+
+	int ping(int t) {
+
+		counter.push(t);
+
+		while(counter.front() < t - 3000){
+			counter.pop();
+		}
+
+		return counter.size();
+	}
+};
