06be075ec6f29505b9a897be5fa4c63ac3e38b3b
mahadi025 <mahadikarimmunif@gmail.com>
2024-01-28 16:53:00 +0600

    added ListNode

--- MiddleOfTheLinkedList.cpp
+++ MiddleOfTheLinkedList.cpp
@@ -1,14 +1,25 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
-class Solution {
+class Solution
+{
 public:
-    ListNode* middleNode(ListNode* head) {
-        ListNode *slow=head,*fast=head;
-        while(fast!=NULL&&fast->next!=NULL){
-            slow=slow->next;
-            fast=fast->next->next;
+    ListNode *middleNode(ListNode *head)
+    {
+        ListNode *slow = head, *fast = head;
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
         }
         return slow;
     }
